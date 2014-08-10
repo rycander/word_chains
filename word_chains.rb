4b5c32a24da5021a306605854800722f2780f17d
Ryan Anderson <esrever.nayr@gmail.com>
2014-08-10 15:59:46 -0700

    added initialize function

--- word_chains.rb
+++ word_chains.rb
@@ -1,2 +1,9 @@
 class WordChains
+  def initialize(dictionary_file_name)
+    @dictionary = File.readlines(dictionary_file_name)
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  wc = WordChains.new(dictionary.txt)
 end
